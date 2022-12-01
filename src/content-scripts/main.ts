05af6dd996959bb10a7d03338ebdc3d1430b36a4
Masaya Kazama <miyaokamiyao@gmail.com>
2022-12-01 22:33:06 +0900

    add content script

--- src/content-scripts/main.ts
+++ src/content-scripts/main.ts
@@ -1,1 +1,20 @@
-console.log("content scripts");
+const textParamKey = "text";
+
+const search = () => {
+  const params = new URL(location.href).searchParams;
+
+  const text = params.get(textParamKey);
+  if (text == null) return;
+
+  const textarea = document.querySelector("textarea");
+  const button = document.querySelector<HTMLButtonElement>("textarea + button");
+  if (!textarea || !button) return;
+
+  textarea.value = text;
+  button.click();
+};
+
+// onMount
+setTimeout(() => {
+  search();
+}, 1000);
